966bc9703b398f7b88aff3598ccef80a8ae1d406
aleksandre jachvadze <155845898+Aleksandre-jachvadze@users.noreply.github.com>
2024-07-24 16:57:30 +0400

    Delete Downloads/planets-fact-site-main directory

--- Downloads/planets-fact-site-main/src/layouts/DefaultLayout/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const LayoutContainer = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`
